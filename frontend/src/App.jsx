13d79e4b5b03090b9c728b91bceecf0d77e982aa
~J.S-13~ <johan.ens757@gmail.com>
2026-05-28 11:49:55 -0500

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,53 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Login       from "./pages/Login";
+import Dashboard   from "./pages/Dashboard";
 import CreateOrder from "./pages/CreateOrder";
-import Tracking from "./pages/Tracking";
-import Repartor from "./pages/Repartor";
-import Rute from "./pages/Rute";
+import Tracking    from "./pages/Tracking";
+import Repartor    from "./pages/Repartor";
+import Rute        from "./pages/Rute";
+import PrivateRoute from "./component/PrivateRoute";
+
+import OrderProvider from "./context/OrderContext";
 
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
 
+        {/* Ruta pública: login */}
         <Route path="/" element={<Login />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* Rutas privadas: requieren sesión activa */}
+        <Route path="/dashboard" element={
+          <PrivateRoute><Dashboard /></PrivateRoute>
+        } />
 
-        <Route path="/create-order" element={<CreateOrder />} />
+        <Route path="/create-order" element={
+          <PrivateRoute>
+            <OrderProvider>
+              <CreateOrder />
+            </OrderProvider>
+          </PrivateRoute>
+        } />
 
-        <Route path="/tracking" element={<Tracking />} />
-		
-		<Route path="/repartor" element={<Repartor />} />
-		
-		<Route path="/rute" element={<Rute />} />
+        <Route path="/tracking" element={
+          <PrivateRoute>
+            <OrderProvider>
+              <Tracking />
+            </OrderProvider>
+          </PrivateRoute>
+        } />
+
+        <Route path="/repartor" element={
+          <PrivateRoute><Repartor /></PrivateRoute>
+        } />
+
+        <Route path="/rute" element={
+          <PrivateRoute><Rute /></PrivateRoute>
+        } />
 
       </Routes>
-
     </BrowserRouter>
   );
 }
